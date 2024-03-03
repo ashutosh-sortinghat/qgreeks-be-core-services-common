e6de74b758d65c7f7cbd3fd07550eeac46fe7be9
RajBuddha <Raaz.Buddha@gmail.com>
2024-03-02 20:43:27 -0800

    Update urls.py

--- qg_screener_app2/urls.py
+++ qg_screener_app2/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home.urls")),
-    path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
 
